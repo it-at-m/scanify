b3904f5868e983c59a475689544fdcd0c4ef47f1
ProjectFlare <94632407+ProjectFlare@users.noreply.github.com>
2026-07-20 17:14:22 +0200

    ci: fix crash

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,13 +40,13 @@ android {
     }
 
     signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
-        }
+    create("release") {
+        keyAlias = keystoreProperties.getProperty("keyAlias")
+        keyPassword = keystoreProperties.getProperty("keyPassword")
+        storeFile = keystoreProperties.getProperty("storeFile")?.let { file(it) }
+        storePassword = keystoreProperties.getProperty("storePassword")
     }
+}
 
     buildTypes {
         release {
